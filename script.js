eda9858b7741fd5884fffbe93c28b352f3ce2f08
IvyNjenga12 <158261629+IvyNjenga12@users.noreply.github.com>
2025-02-26 05:02:46 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let userName = "Guest";
 let menuItems = ["Nyama Choma", "Ugali & Sukuma", "Chapati & Beans", "Tilapia Fry", "Mutura"];
-
 document.getElementById("btnEnterName").addEventListener("click", function () {
     let nameInput = prompt("Please enter your first name:");
 
@@ -10,35 +9,28 @@ document.getElementById("btnEnterName").addEventListener("click", function () {
         userName = nameInput;
     }
 
-    alert(`Welcome to EatNow Cafe, ${userName}!`);
+    alert("Welcome to EatNow Cafe, " + userName + "!");
 });
-
 document.getElementById("btnViewMenu").addEventListener("click", function () {
     let menuText = "Menu:\n";
-    
-    menuItems.forEach((item, index) => {
-        menuText += `${index + 1}. ${item}\n`; // Formatting menu list for alert
-        console.log(`${index + 1}. ${item}`);  // Printing to console in numbered format
+    menuItems.forEach(function(item, index) {
+        menuText += (index + 1) + ". " + item + "\n";
+        console.log((index + 1) + ". " + item); 
     });
 
     alert(menuText);
 });
-
 document.getElementById("btnAddDish").addEventListener("click", function () {
     let newDish = prompt("Suggest a new dish to add to the menu:");
-
     if (newDish === "" || newDish === null) {
         console.log("No new dish added. Please enter a valid dish name.");
         return;
     }
-
     menuItems.push(newDish);
-
     let updatedMenu = "Updated Menu:\n";
-    menuItems.forEach((item, index) => {
-        updatedMenu += `${index + 1}. ${item}\n`;
-        console.log(`${index + 1}. ${item}`); // Print updated menu to console
+    menuItems.forEach(function(item, index) {
+        updatedMenu += (index + 1) + ". " + item + "\n";
+        console.log((index + 1) + ". " + item);
     });
-
     alert(updatedMenu);
 });
